e53d8c9825b20205ec91017c99a7344d611fb519
AdelaMester <adelamester@yahoo.co.uk>
2021-09-05 16:24:53 +0000

    First request created for Oauth login

--- crudapp/urls.py
+++ crudapp/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("profile/", views.profile, name="profile"),
     path("updateprofile/", views.updateprofile, name="updateprofile"),
-    path("deleteinformation/", views.deleteinformation, name="deleteinformation")
+    path("deleteinformation/", views.deleteinformation, name="deleteinformation"),
+    path("request_identity/", views.request_identity, name="request_identity"),
 ]
--- crudapp/views.py
+++ crudapp/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 import os
+from uuid import uuid4
+import urllib.parse
 
 client_id = os.environ.get('client_id')
 client_secret = os.environ.get('client_secret')
+redirect_uri
 
 
 # Create your views here.
 def index(request):
-    print(client_id)
     return render(request, "crudapp/index.html")
 
 
@@ -23,3 +25,18 @@ def updateprofile(request):
 def deleteinformation(request):
     if request.method == 'GET':
         return render(request, "crudapp/deleteinformation.html")
+
+def request_identity(request):
+    state = str(uuid4())
+    params = {"client_id": client_id,
+              "response_type": "code",
+              "redirect_uri": redirect_uri,
+              "state": state,
+              "duration": "temporary",
+              "scope": "user"
+              }
+    return HttpResponseRedirect('https://github.com/login/oauth/authorize?' + urllib.parse.urlencode(params))
+
+def callback(request):
+    if request.method == 'GET':
+        return render(request, "crudapp/home.html")
