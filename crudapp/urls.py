c87a2dfea31987d7a8c0c47ace66c81a8caac715
AdelaMester <adelamester@yahoo.co.uk>
2021-09-05 15:16:08 +0000

    Created the routes for the three templates

--- crudapp/urls.py
+++ crudapp/urls.py
@@ -3,5 +3,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("profile/", views.profile, name="profile"),
+    path("update/", views.updateprofile, name="updateprofile"),
+    path("delete/", views.deleteinformation, name="deleteinformation")
 ]
--- crudapp/views.py
+++ crudapp/views.py
@@ -4,3 +4,15 @@ from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
 def index(request):
     return render(request, "crudapp/home.html")
+
+def profile(request):
+    if request.method == 'GET':
+        return render(request, "crudapp/profile.html")
+
+def updateprofile(request):
+    if request.method == 'GET':
+        return render(request, "crudapp/updateinformation.html")
+
+def deleteinformation(request):
+    if request.method == 'GET':
+        return render(request, "crudapp/deleteinformation.html")
