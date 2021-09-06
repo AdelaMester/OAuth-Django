67b4d2cc72617852485a4bbdfcd768936e8be1d4
AdelaMester <adelamester@yahoo.co.uk>
2021-09-06 18:04:54 +0000

    Save username only once

--- crudapp/views.py
+++ crudapp/views.py
@@ -60,8 +60,11 @@ def callback(request):
         print(name)
         conn = psycopg2.connect(os.environ.get('DATABASE_URL'))
         cur = conn.cursor()
-        cur.execute("INSERT INTO users (username, access_token) VALUES (%s, %s)", (name, at));
-        conn.commit()
+        cur.execute("SELECT * FROM users WHERE username=%s", (username,))
+        row = cur.fetchone()
+        if not row[0]:
+            cur.execute("INSERT INTO users (username, access_token) VALUES (%s, %s)", (name, at));
+            conn.commit()
         cur.close()
         conn.close()
         return render(request, "crudapp/home.html", {
