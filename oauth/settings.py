85084992acd991d43122982461b18045abae1f2d
AdelaMester <adelamester@yahoo.co.uk>
2021-09-07 18:19:47 +0000

    Updated environment variables

--- oauth/settings.py
+++ oauth/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kggw&t@+b=!#+k2v+vr)_1ot=0-86%l1(mhv7e$5w#trf9sanr'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
